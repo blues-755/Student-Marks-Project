32a70419b1f46afd2f2f2a88daa49422100ed1f0
LTDO <lungatshila10@gmail.com>
2021-06-08 17:04:28 +0200

    added User entity members and Builder pattern implementation

--- src/main/java/za/ac/cput/Entity/User.java
+++ src/main/java/za/ac/cput/Entity/User.java
@@ -1,4 +1,69 @@
 package za.ac.cput.Entity;
 
+
 public class User {
+
+    String id, firstName, lastName, middleName;
+    int age;
+
+    public User(Builder builder) {
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.middleName = builder.middleName;
+        this.age = builder.age;
+    }
+
+    public static class Builder {
+
+        String id, firstName, lastName, middleName;
+        private int age;
+
+        public Builder setId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setMiddleName(String middleName) {
+            this.middleName = middleName;
+            return this;
+        }
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public User builder(){
+            return new User(this);
+        }
+
+        //takes user attributes and initializes them to builder class.
+        public Builder copy(User user){
+            this.id = user.id;
+            this.firstName = user.firstName;
+            this.lastName = user.lastName;
+            this.middleName = user.middleName;
+            this.age = user.age;
+            return this;
+        }
+    }
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
