0cf86e58b4b5161af444b1de44ee53469165b878
Marcoo0198 <219049505@mycput.ac.za>
2021-06-11 16:41:24 +0200

    creation of level class

---
+++ src/main/java/za/ac/cput/Entity/LevelClass.java
@@ -0,0 +1,54 @@
+/*
+  LevelClass.java
+  author:  marco Mulonday 219049505
+  date 10/06/2021
+ */
+
+
+package za.ac.cput.Entity;
+
+public class LevelClass {
+    private int levelID;
+    private String levelName, grade;
+
+
+    public LevelClass(builder builder) {
+        this.levelID = builder.levelID;
+        this.levelName = builder.levelName;
+        this.grade = builder.grade;
+    }
+
+
+    public static class builder {
+        private int levelID;
+        private String levelName, grade;
+
+        public builder setLevelID(int levelID) {
+            this.levelID = levelID;
+            return this;
+        }
+
+        public builder setLevelName(String levelName) {
+            this.levelName = levelName;
+            return this;
+        }
+
+        public builder setGrade(String grade) {
+            this.grade = grade;
+            return this;
+        }
+
+        public LevelClass build() {
+
+            return new LevelClass(this);
+        }
+
+        public builder copy(LevelClass levelClass) {
+
+            this.levelID = levelClass.levelID;
+            this.levelName = levelClass.levelName;
+            this.grade = levelClass.grade;
+            return this;
+        }
+    }
+}
