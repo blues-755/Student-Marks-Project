a36632af2d0488f5c5e2e0c993421b4cd12d5fbb
Marco0198 <81382003+Marco0198@users.noreply.github.com>
2021-06-11 23:40:17 +0200

    Update LevelClassTest.java

--- src/test/java/za/ac/cput/Entity/LevelClassTest.java
+++ src/test/java/za/ac/cput/Entity/LevelClassTest.java
@@ -2,6 +2,50 @@ package za.ac.cput.Entity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class LevelClassTest {
 
-}
+/*
+ author marco mulonday
+ 219049505
+ */
+import za.ac.cput.Entity.LevelClass;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+
+    class LevelClassTest {
+        private LevelClass  levelclass1;
+        private LevelClass  levelclass2;
+        private LevelClass  levelclass3;
+        @BeforeEach
+        void setUp() {
+            this.levelclass1 = new LevelClass();
+            this.levelclass2 = new LevelClass();
+            this.levelclass3= new LevelClass();
+            this.levelclass1 = this.levelclass2;
+        }
+        // testing Identity
+        @Test
+        public void testIdentity() {
+            Assertions.assertSame(this.levelclass1 ,this.levelclass2);;
+        }
+        // testing equality
+        @Test
+        public void testEquality() {
+            Assertions.assertEquals(this.levelclass1 ,this.levelclass2);
+        }
+        // test for faill test
+        @Test
+        public void testFail() {
+            Assertions.assertEquals(this.levelclass1 ,this.levelclass2);
+            Assertions.fail("this will faill");
+        }
+        // test for disable test
+
+        @Disabled("Disabled until CustomerService is up!")
+        @Test
+        void testCustomerServiceGet() {
+            Assertions.assertEquals(this.levelclass1 ,this.levelclass2);
+        }
+    }
