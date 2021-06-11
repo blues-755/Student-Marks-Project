93c8aaf3c7c818e3308f00683ab48892e58c8909
Marcoo0198 <219049505@mycput.ac.za>
2021-06-11 23:13:20 +0200

    i add 2 new file on the project

---
+++ src/main/java/za/ac/cput/Factory/LevelClassFactory.java
@@ -0,0 +1,34 @@
+
+/*
+  LevelClass.java
+  author:  marco Mulonday 219049505
+  date 10/06/2021
+ */
+
+package za.ac.cput.Factory;
+import za.ac.cput.Entity.LevelClass;
+
+
+import java.util.UUID;
+
+public class LevelClassFactory {
+
+    public static LevelClass build(String levelName, String grade) {
+
+        try {
+            if (levelName.isEmpty() || grade.isEmpty()) {
+                return null;
+            }
+        } catch (NullPointerException e) {
+            System.out.println("please Enter all values");
+        }
+
+
+        String levelID = UUID.randomUUID().toString();
+        return new LevelClass.builder()
+                .setLevelID(levelID)
+                .setLevelName(levelName)
+                .setGrade(grade)
+                .builder();
+    }
+}
