700936c8a0a8b0b4785909c0c27f859b7126fc24
LTDO <lungatshila10@gmail.com>
2021-08-17 01:38:47 +0200

    added author info

--- src/main/java/za/ac/cput/controller/HomeController.java
+++ src/main/java/za/ac/cput/controller/HomeController.java
@@ -1,4 +1,11 @@
 package za.ac.cput.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HomeController {
+    @RequestMapping({"/", "/home"})
+    public String home() {
+        return "Student Application Home Page";
+    }
 }
